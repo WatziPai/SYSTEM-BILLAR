a4c9aaba4bb1ef0ef924561f83c0ca76cb3125f1
Manuel Aliaga <2023110541@udh.edu.pe>
2026-06-04 15:30:06 -0500

    Fix: base path para corregir estilos en github pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  // Agregamos esta línea para corregir las rutas en GitHub Pages:
+  base: '/SYSTEM-BILLAR/',
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
